2a162c92545a0bb3711ec9e317e00b7335aa8364
angryfrenchguy <angryfrenchguy@gmail.com>
2017-05-02 00:53:25 -0400

    peut-être que jai un datepicker?

--- code/code.js
+++ code/code.js
@@ -1,17 +1,3 @@
-//function datePicker() {
-//    
-//    $('.datepicker').datepicker({
-//        firstday: 0, //met le début de la semaine le dimanche
-////        dateFormat: 'd MM yy',
-//        altField: '#semaineAU',
-//        altFormat: 'd MM yy'
-//    });
-//    
-////    var zzz = this;
-//    
-//    $('.datepicker').datepicker('setDate', zzz);
-//}
-
 function ediDate() {
     $('.editdate').editable('http://localhost:8888/feuille_de_route/retourjeditable.php', {
         onblur: 'submit',
@@ -27,5 +13,46 @@ function ediDate() {
         }
     }
 )
+}
+
+function semaine() {
+    
+    $('#semaineDU').dblclick(function() {
+        ajouterInput();
+    })
+    
+    $('#semaineDU').focusout(function() {
+        alert('yep.');
+        
+    var valeur = $('#semaineDU').find('input').val();
+    
+//    moment(valeur, 'L').isValid() ? valeur = moment(valeur, 'L').format('D MMMM YYYY') : valeur = "";
+        
+        
+    
+    $('#semaineDU').html(moment(valeur).startOf('week').format('D MMMM YYYY'));
+    $('#semaineAU').html(moment($('#semaineDU').html(), 'D MMMM YYYY').add(6, 'd').format('D MMMM YYYY'));
+       
+//    document.getElementById('semaineDU').removeChild( document.getElementById('semaineDU').children[0]);
+        
+    });
+}
+
+function ajouterInput () {
+    
+    document.getElementById('semaineDU').innerHTML = "";
+
+    
+    if (document.getElementById('semaineDU').hasChildNodes() !== true) {
+    
+    
+    
+    var element = document.createElement('input');
+    
+    element.type = "date";
+    
+    var semaineDU = document.getElementById('semaineDU');
+    semaineDU.appendChild(element);
+    }
 }
     
