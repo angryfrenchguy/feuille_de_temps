9593611631717b20bf7cd6c58cab3945581ca4cc
angryfrenchguy <angryfrenchguy@gmail.com>
2017-05-06 17:14:53 -0400

    tentative d'ajouter type:date on the fly

--- code/code.js
+++ code/code.js
@@ -1,3 +1,109 @@
+function edit() {
+    $('.edit').editable(function(value, settings) {
+        
+        var table = document.getElementById('tbodyID');
+        var lignes = table.rows.length;
+        var miarrey = [];
+        
+        console.log($(this).attr('class'));
+        $(this).attr('class') == 'edit editdate' ? $(this).find('input').attr('type', 'date') : void 0;
+        console.log($(this).find('input'));
+        
+        for(i = 0; i < lignes; i++) {
+            
+            var ligne = table.rows[i];
+            
+            var colonneDate = ligne.cells[0];
+            var colonneContrat = ligne.cells[1];
+            var colonneClient = ligne.cells[2];
+            var colonneBus = ligne.cells[3];
+            var colonneOdoIN = ligne.cells[4];
+            var colonneOdoOUT = ligne.cells[5];
+            var colonneOdoTOTAL = ligne.cells[6]; 
+            var colonneTempsIN = ligne.cells[7];
+            var colonneTempsOUT = ligne.cells[8];
+            var colonneTempsTOTAL = ligne.cells[9];
+            var colonneEtat = ligne.cells[10];
+            var idUnique = $('#semaineDU').html();
+            
+            var id = i;
+            var date = colonneDate.getElementsByTagName('input')[0] ? colonneDate.getElementsByTagName('input')[0].value : moment(ligne.cells[0].innerHTML, 'LL').format('L');
+            var contrat = colonneContrat.getElementsByTagName('input')[0] ? colonneContrat.getElementsByTagName('input')[0].value : ligne.cells[1].innerHTML;
+            var client = colonneClient.getElementsByTagName('input')[0] ? colonneClient.getElementsByTagName('input')[0].value : ligne.cells[2].innerHTML;
+            var bus = colonneBus.getElementsByTagName('input')[0] ? colonneBus.getElementsByTagName('input')[0].value : ligne.cells[3].innerHTML;
+            var odoIN = colonneOdoIN.getElementsByTagName('input')[0] ? colonneOdoIN.getElementsByTagName('input')[0].value : ligne.cells[4].innerHTML;
+            var odoOUT = colonneOdoOUT.getElementsByTagName('input')[0] ? colonneOdoOUT.getElementsByTagName('input')[0].value : ligne.cells[5].innerHTML;
+            var odoTOTAL = colonneOdoTOTAL.getElementsByTagName('input')[0] ? colonneOdoTOTAL.getElementsByTagName('input')[0].value : ligne.cells[6].innerHTML;
+            var tempsIN = colonneTempsIN.getElementsByTagName('input')[0] ? colonneTempsIN.getElementsByTagName('input')[0].value : ligne.cells[7].innerHTML;
+            var tempsOUT = colonneTempsOUT.getElementsByTagName('input')[0] ? colonneTempsOUT.getElementsByTagName('input')[0].value : ligne.cells[8].innerHTML;
+            var tempsTOTAL = colonneTempsTOTAL.getElementsByTagName('input')[0] ? colonneTempsTOTAL.getElementsByTagName('input')[0].value : ligne.cells[9].innerHTML;
+            var  etat= colonneEtat.getElementsByTagName('input')[0] ? colonneEtat.getElementsByTagName('input')[0].value : ligne.cells[10].innerHTML;
+    
+            var semaineDU = moment($('#semaineDU').html(), 'LL').format('L');
+            var semaineAU = moment($('#semaineAU').html(), 'LL').format('L');
+            
+            if(moment(date, 'L').isBetween(semaineDU, semaineAU,'week', []) == false) {
+                alert("Ta date n'est pas dans la bonne semaine, dude.");
+                colonneDate.getElementsByTagName('input')[0].value = "Oups. Date invalide";
+                throw new Error("date invalide");
+            }
+            
+            var idx = i < 10 ? '0' + i : i;
+            
+            idUnique = faireUnID(idUnique, idx);
+            
+            var ligneObjet = {
+                idUnique: idUnique,
+                date: date,
+                contrat: contrat,
+                client: client, 
+                bus: bus,
+                odoIN: odoIN,
+                odoOUT: odoOUT,
+                odoTOTAL: odoTOTAL,
+                tempsIN: tempsIN,
+                tempsOUT: tempsOUT,
+                tempsTOTAL: tempsTOTAL,
+                etat: etat
+            };
+            
+            miarrey.push(ligneObjet);
+};
+        $.ajax({
+            url: 'http://localhost:8888/sauvegarde.php',
+            type: 'POST',
+            data: { data: miarrey },
+            success: function(data) {
+//                data = JSON.parse(data);
+//                for(i = 0; i < data.length; i++) {
+//                    moment(data[i].date, 'L').isValid() ? document.getElementById('tbodyID').rows[i].cells[0].innerHTML = moment(data[i].date, 'L').format('LL') : document.getElementById('tbodyID').rows[i].cells[0].innerHTML = "";
+//                };
+                console.log(data);
+            },
+            error: function(data, textStatus, errorThrown) {
+                console.log("Statut: " + textStatus);
+                console.log("Erreur: " + errorThrown);
+            }
+        })
+        
+        return value;
+        
+    }, {
+        event: 'dblclick',
+        onblur: 'submit',
+        placeholder: ''
+    });
+}
+                        
+function faireUnID(date, rang) {
+        var table = document.getElementById('tbodyID');
+        var lignes = table.rows.length;
+        var dateFormat = moment(date, 'LL').format('YYYYww');
+        var idUnique = dateFormat + rang;
+        
+        return idUnique;
+    }                        
+
 function semaine() {
     
     $('#semaineDU').on('touchstart dblclick', function() {
@@ -10,7 +116,7 @@ function semaine() {
         
         var element = document.createElement('input');
         element.type = "date";
-        element.value = moment(def, 'D MMMM YYYY').format('L');
+        element.value = moment(def, 'LL').format('L');
         element.autofocus = true;
         
         var semaineDU = document.getElementById('semaineDU');
@@ -22,8 +128,8 @@ function semaine() {
     $('#semaineDU').focusout(function() {
         var valeur = $('#semaineDU').find('input').val();
         
-        $('#semaineDU').html(moment(valeur).startOf('week').format('D MMMM YYYY'));
-        $('#semaineAU').html(moment($('#semaineDU').html(), 'D MMMM YYYY').add(6, 'd').format('D MMMM YYYY'));
+        $('#semaineDU').html(moment(valeur).startOf('week').format('LL'));
+        $('#semaineAU').html(moment($('#semaineDU').html(), 'LL').add(6, 'd').format('LL'));
     });
 }
     
